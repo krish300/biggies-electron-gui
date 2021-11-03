0c8cb1441573d2008d5f7c384be67fef6abdac2e
KRISHNA TEJA S <krishna.learn@outlook.com>
2021-11-03 13:21:58 +0530

    intial checkin

---
+++ src/renderer.js
@@ -0,0 +1,239 @@
+import $ from 'jquery';
+
+// we also need to process some styles with webpack
+import fontawesome from '@fortawesome/fontawesome';
+import { faCloudUploadAlt, faExclamationCircle, faCheckCircle } from '@fortawesome/fontawesome-free-solid';
+fontawesome.library.add(faCloudUploadAlt);
+fontawesome.library.add(faExclamationCircle);
+fontawesome.library.add(faCheckCircle);
+import './styles/index.scss';
+
+const drop = document.querySelector('input');
+const filesInput = document.querySelector('#files');
+const errorArea = document.querySelector('.error-toast');
+const notificationArea = document.querySelector('.notification-toast');
+
+const handleIn = () => {
+
+  $('.drop').css({
+    border: '4px dashed #3023AE',
+    background: 'rgba(0, 153, 255, .05)'
+  });
+
+  $('.cont').css({
+    color: '#3023AE'
+  });
+
+};
+
+const handleOut = () => {
+
+  $('.drop').css({
+    border: '3px dashed #DADFE3',
+    background: 'transparent'
+  });
+
+  $('.cont').css({
+    color: '#8E99A5'
+  });
+
+};
+
+const inEvents = ['dragenter', 'dragover'];
+const outEvents = ['dragleave', 'dragend', 'mouseout', 'drop'];
+
+inEvents.forEach(event => drop.addEventListener(event, handleIn));
+outEvents.forEach(event => drop.addEventListener(event, handleOut));
+
+const handleFileSelect = event => {
+  const files = event.target.files;
+
+  for (let file of files) {
+
+    // Only process excel files.
+    if (!file.type.match('officedocument.*')) {
+      continue;
+    }
+
+    window.postMessage({
+      type: 'file-dropped',
+      data: file.path
+    }, '*');
+  }
+
+  event.preventDefault();
+  event.stopPropagation();
+
+};
+
+filesInput.addEventListener('change', handleFileSelect);
+
+const $progress = $('.progress'),
+  $bar = $('.progress__bar'),
+  $text = $('.progress__text'),
+  orange = 30,
+  yellow = 55,
+  green = 85;
+
+const resetColors = () => {
+
+  $bar.removeClass('progress__bar--green')
+    .removeClass('progress__bar--yellow')
+    .removeClass('progress__bar--orange')
+    .removeClass('progress__bar--blue');
+
+  $progress.removeClass('progress--complete');
+
+};
+
+const update = (percent) => {
+
+  percent = parseFloat( percent.toFixed(1) );
+
+  $text.find('em').text( percent + '%' );
+
+  if( percent >= 100 ) {
+
+    percent = 100;
+    $progress.addClass('progress--complete');
+    $bar.addClass('progress__bar--blue');
+    $text.find('em').text( 'Complete' );
+
+  } else {
+
+    if( percent >= green ) {
+      $bar.addClass('progress__bar--green');
+    }
+
+    else if( percent >= yellow ) {
+      $bar.addClass('progress__bar--yellow');
+    }
+
+    else if( percent >= orange ) {
+      $bar.addClass('progress__bar--orange');
+    }
+
+  }
+
+  $bar.css({ width: percent + '%' });
+
+};
+
+const processStartHandler = () => {
+
+  $progress.addClass('progress--active');
+  $progress.show();
+  $('.wrapper').hide();
+
+};
+
+const progressHandler = percentage => update(percentage);
+
+const processCompletedHandler = ({ processedItemsCount, incompatibleItems, erroneousItems, logFilePath }) => {
+
+  $(notificationArea).find('.text').text(
+    [
+      `${processedItemsCount} item(s) successfully processed,`,
+      `${incompatibleItems.length} item(s) skipped,`,
+      `${erroneousItems.length} item(s) erroneous,`,
+      `Log file ${logFilePath} is written on disk.`
+    ].join('\r\n')
+  );
+
+  $(notificationArea).show().animate({
+    top: '10%'
+  }, 'slow');
+
+};
+
+const processErrorHandler = data => {
+
+  const oldText = $(errorArea).find('.text').text();
+
+  $(errorArea).find('.text').text(
+    [
+      `${oldText}`,
+      `${data.itemInfo} ${data.statusText}`
+    ].join('\r\n')
+  );
+
+  $(errorArea).show().animate({
+    bottom: '10%'
+  }, 'slow');
+
+};
+
+const fileErrorHandler = data => {
+
+  $(errorArea).find('.text').text(`${data}`);
+
+  $(errorArea).show().animate({
+    bottom: '10%'
+  }, 'slow');
+
+};
+
+const resetProcess = () => {
+
+  resetColors();
+  update(0);
+  $('.wrapper').show();
+  $progress.hide();
+
+};
+
+const errorAreaClickHandler = () => {
+
+  $(errorArea).animate({
+    bottom: 0
+  }, 'slow', function() { $(this).hide().find('.text').text('')});
+
+};
+
+const notificationAreaClickHandler = () => {
+
+  $(notificationArea).animate({
+    top: 0
+  }, 'slow', function() { $(this).hide().find('.text').text('')});
+
+  errorAreaClickHandler();
+  resetProcess();
+};
+
+errorArea.addEventListener('click', errorAreaClickHandler);
+
+notificationArea.addEventListener('click', notificationAreaClickHandler);
+
+const disableDrop = event => {
+  if(event.target !== filesInput) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+};
+
+// Prevent loading a drag-and-dropped file
+['dragover', 'drop'].forEach(event => {
+  document.addEventListener(event, disableDrop);
+});
+
+window.addEventListener('message', event => {
+  const message = event.data;
+  const { data, type } = message;
+
+  switch (type) {
+    case 'process-started':
+      processStartHandler();
+      break;
+    case 'process-completed':
+      processCompletedHandler(data);
+      break;
+    case 'progress':
+      progressHandler(data);
+      break;
+    case 'process-error':
+      processErrorHandler(data);
+      break;
+    case 'file-error':
+      fileErrorHandler(data);
+  }
+});
---
+++ src/utils/processItems.js
@@ -0,0 +1,152 @@
+import path from 'path';
+import { createWriteStream, mkdir } from 'fs';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+import fetch from 'electron-fetch';
+import { URL } from 'url';
+import xlsx from 'node-xlsx';
+import isUrl from 'is-url';
+import mime from 'mime-types';
+
+import generateFileName from './generateFileName';
+const streamPipeline = promisify(pipeline);
+
+let initialItemsLength;
+let processedItemsCount;
+let incompatibleItems = [];
+let erroneousItems = [];
+
+const _resetProcessData = () => {
+  initialItemsLength = 0;
+  processedItemsCount = 0;
+  incompatibleItems.length = 0;
+  erroneousItems.length = 0;
+};
+
+const processItem = async (item, outputPath) => {
+
+  const [ itemUrl, newName, subFolderName ] = item;
+  const url = new URL(itemUrl);
+  const itemName = newName ? `${newName}${path.extname(url.pathname)}` : path.basename(url.pathname);
+
+  // usage of 'If-None-Match' header is just to force the server not to return an 304 since electron net does not
+  // correctly return content-type headers when converting an 304 to 200 internally
+  // see issues https://github.com/electron/electron/issues/27895, https://github.com/electron/electron/pull/21552
+  // and https://github.com/electron/electron/issues/20631
+  const response = await fetch(itemUrl, {headers: {'If-None-Match': null}});
+
+  if (response.ok) {
+    if (subFolderName) {
+      await mkdir(`${outputPath}/${subFolderName}`, { recursive: true }, () => {});
+    }
+
+    const contentType = response.headers.get('content-type');
+    const extension = mime.extension(contentType);
+    const fileName = generateFileName(itemName, extension);
+    // file system flag 'wx' stands for:
+    // Open file for writing. The file is created (if it does not exist), but fails if the path exists. (x causes to throw)
+    const dest = createWriteStream(path.join(outputPath, subFolderName || '', fileName), {flags: 'wx'});
+
+    try {
+      await streamPipeline(response.body, dest);
+    } catch (error) {
+      throw {
+        statusText: error.message,
+        itemInfo: error.path
+      }
+    }
+  } else {
+    throw {
+      status: response.status,
+      statusText: response.statusText,
+      itemInfo: url.href
+    }
+  }
+};
+
+const processItems = async (rowItems, outputPath, win) => {
+  const itemsLength = rowItems.length;
+
+  for (let i = 0; i < itemsLength; i += 20) {
+    const requests = rowItems.slice(i, i + 20).map(item => {
+      return processItem(item, outputPath)
+        .then(() => {
+          const unprocessedItems = erroneousItems.length + incompatibleItems.length;
+          const percentage = Math.abs(++processedItemsCount / (initialItemsLength - unprocessedItems)) * 100;
+
+          win.webContents.send('main-message', {
+            type: 'progress',
+            data: percentage
+          });
+        })
+        .catch(err => {
+          erroneousItems.push(err.itemInfo);
+
+          win.webContents.send('main-message', {
+            type: 'process-error',
+            data: err
+          });
+        });
+    });
+
+    await Promise.allSettled(requests);
+  }
+
+  const logFileStream = createWriteStream(path.join(
+    outputPath,
+    `excel-parser-processor-log${Date.now()}.txt`)
+  );
+
+  logFileStream.write(
+    [
+      `Processed Items Count: ${processedItemsCount}`,
+      '-----',
+      `Incompatible Items Count: ${incompatibleItems.length};`,
+      `${incompatibleItems.join('\r\n')}`,
+      '-----',
+      `Erroneous Items Count: ${erroneousItems.length};`,
+      `${erroneousItems.join('\r\n')}`,
+      '-----'
+    ].join('\r\n'), 'utf8');
+
+  logFileStream.on('finish', () => {
+    win.webContents.send('main-message', {
+      type: 'process-completed',
+      data: {
+        processedItemsCount,
+        incompatibleItems,
+        erroneousItems,
+        logFilePath: logFileStream.path
+      }
+    });
+  });
+
+  logFileStream.end();
+};
+
+export const processFile = async (filePath, outputPath, browserWindow) => {
+
+  _resetProcessData();
+
+  const workSheetsFromFile = xlsx.parse(filePath);
+  const dataRows = workSheetsFromFile.flatMap(page => page.data).filter(item => item.length);
+  const validRows = dataRows.filter(row => row.some(text => isUrl(text)));
+
+  incompatibleItems = dataRows.filter(row => !row.some(text => isUrl(text)));
+
+  initialItemsLength = validRows.length;
+
+  if (initialItemsLength) {
+    browserWindow.webContents.send('main-message', {
+      type: 'process-started'
+    });
+
+    await processItems(validRows, outputPath, browserWindow);
+  } else {
+    browserWindow.webContents.send('main-message', {
+      type: 'file-error',
+      data: 'No item to process'
+    });
+  }
+
+};
